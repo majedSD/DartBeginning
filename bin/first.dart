bb7363072aadb3c46a735c87f3f99270f78166b3
Ekushey Computer <majedulislam117@gmail.com>
2023-08-14 10:52:26 +0600

    updated

--- bin/first.dart
+++ bin/first.dart
@@ -1,5 +1,11 @@
 void main(){
- for (int i=0;i<100;i++){
-  print('This is cosmatic:$i');
- }
+ /// how to creat a map using dart constructor creat map using dart constructor
+  var mp=new Map();
+  mp[1]='Abdul mutaleb';
+  mp[2]='Ayon kumar bonik';
+  mp[3]='Mustafizur Rahman';
+  mp.addAll({4:'afjajf',5:'wiyriuqy',6:'qfriqu',7:'ahfauah'});
+  mp.remove(1);
+  mp.remove(2);
+  print(mp);
 }
