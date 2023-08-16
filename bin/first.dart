7235d8ca1c6b58bbcbf01065540093627bf68317
Ekushey Computer <majedulislam117@gmail.com>
2023-08-16 15:01:10 +0600

    updated

--- bin/first.dart
+++ bin/first.dart
@@ -1,11 +1,17 @@
+class Constructor{
+   late String name;
+   late String Age;
+   late int phone;
+  Constructor(String a,String b,int c){
+    print('Printing the constructor');
+    this.name=a;
+    this.Age=b;
+    this.phone=c;
+  }
+}
 void main(){
- /// how to creat a map using dart constructor creat map using dart constructor
-  var mp=new Map();
-  mp[1]='Abdul mutaleb';
-  mp[2]='Ayon kumar bonik';
-  mp[3]='Mustafizur Rahman';
-  mp.addAll({4:'afjajf',5:'wiyriuqy',6:'qfriqu',7:'ahfauah'});
-  mp.remove(1);
-  mp.remove(2);
-  print(mp);
+  Constructor p1=new Constructor('Majedul Islam','1461641',57285727);
+  Constructor p2=new Constructor('Rafat j.m','572572',257287524);
+  print(p1.name);
+  print(p2.name);
 }
