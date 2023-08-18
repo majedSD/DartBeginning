6e32a47522050a2b0b5b9780364f7bfaad8bbe8b
Ekushey Computer <majedulislam117@gmail.com>
2023-08-18 15:50:07 +0600

    updated

--- bin/first.dart
+++ bin/first.dart
@@ -1,3 +1,7 @@
+
+///>>>>>>>>Assignmet Programme code>>>>>>>>>>>>>>
+///................Majedul Islam..................
+
 class Car{
   String brand;
   String model;
@@ -7,27 +11,27 @@ class Car{
   Car(this.brand,this.model,this.year,this.milesDriven){
     numberOfCars++;
   }
-  drive(double miles){
-   milesDriven+=miles;
+  void drive(double miles){
+    milesDriven+=miles;
   }
-  getMilesDriven(){
-     return (this.milesDriven).toStringAsFixed(0);
+ int getMilesDriven(){
+    return (this.milesDriven).toInt();
   }
-  getBrand(){
+ String getBrand(){
     return this.brand;
   }
-  getModel(){
+ String getModel(){
     return this.model;
   }
-  getYear(){
-   return this.year;
+  int getYear(){
+    return this.year;
   }
   int getAge(){
     return (2023-this.year);
   }
 }
 void main(){
-  Car Car1=Car('Toyota','Carmy',2020,10000);
+  Car Car1=Car('Toyota','Camry',2020,10000);
   print("Car ${Car.numberOfCars}: ${Car1.getBrand()} ${Car1.getModel()} ${Car1.getYear()} Miles: ${Car1.getMilesDriven()} Age: ${Car1.getAge()}");
   Car Car2=Car('Honda','Civic',2018,8000);
   print("Car ${Car.numberOfCars}: ${Car2.getBrand()} ${Car2.getModel()} ${Car2.getYear()} Miles: ${Car2.getMilesDriven()} Age: ${Car2.getAge()}");
